4f8dfc9169f765bc262fd147d4e245691315c6af
Julian <vilaj46@gmail.com>
2022-03-28 08:51:56 -0400

    Updated Navbar/Header.

--- studio/previews/HeaderPreview.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Header } from "union-gables-components";
-
-function HeaderPreview(props) {
-  console.log(props);
-  return <span>Header</span>;
-}
-
-export default HeaderPreview;
