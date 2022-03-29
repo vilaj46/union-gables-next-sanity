5bb69de3c25e02b9415c2862d44021d7db832b4f
Julian <vilaj46@gmail.com>
2022-03-29 09:41:37 -0400

    Header successful implemented.

--- web/components/SlideShow/utilities/createSlideImages.js
+++ web/components/SlideShow/utilities/createSlideImages.js
@@ -1,10 +1,9 @@
 import urlFor from "../../../clientUtils/urlFor";
 
-function createSlideImages(slides) {
+function createSlideImages(slides = []) {
   return slides.map((img) => {
     const imgCDN = urlFor(img);
     const { src, alt } = imgCDN;
-    console.log(imgCDN);
     return {
       src,
       alt,
--- web/pages/[[...slug]].js
+++ web/pages/[[...slug]].js
@@ -1,5 +1,7 @@
 import React from "react";
 import groq from "groq";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import BlockContent from "@sanity/block-content-to-react";
 
 // No drop down for rooms in header.
@@ -22,19 +24,32 @@ import BlockRenderer from "../renderers/BlockRenderer";
 import ImageRenderer from "../renderers/ImageRenderer";
 import BreakRenderer from "../renderers/BreakRenderer";
 import ObjectRenderer from "../renderers/ObjectRenderer";
+import SlideShowRenderer from "../renderers/SlideShowRenderer";
 import DarkenSliderRenderer from "../renderers/DarkenSliderRenderer";
 
 // Components
+import Navbar from "../components/Navbar";
+import SlideShow from "../components/SlideShow";
 import MainContainer from "../components/MainContainer";
+
+// Utilities
+import findSlideShow from "../components/SlideShow/utilities/findSlideShow";
+import createSlideImages from "../components/SlideShow/utilities/createSlideImages";
 
 function Pages({
   page = {
     body: [],
   },
+  headerLinks,
 }) {
+  const router = useRouter();
   const { body } = page;
+  const foundSlideShow = findSlideShow(body) || [];
+  const slides = createSlideImages(foundSlideShow.slides);
   return (
     <main>
+      <SlideShow slides={slides} />
+      <Navbar links={headerLinks} router={router} LinkComponent={Link} />
       <MainContainer>
         <BlockContent
           blocks={body || []}
@@ -44,6 +59,7 @@ function Pages({
               image: ImageRenderer,
               break: BreakRenderer,
               redBallList: ObjectRenderer,
+              // slideShow: SlideShowRenderer,
               darkenSlider: DarkenSliderRenderer,
             },
           }}
@@ -58,6 +74,8 @@ const query = groq`*[_type == "page" && slug.current == $slug][0]{
   title,
   body
 }`;
+
+const headerQuery = groq`*[_type == "header"]`;
 
 export async function getStaticPaths() {
   const paths = await client.fetch(groq`*[_type == "page"]`);
@@ -74,18 +92,23 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const { slug = "" } = context.params;
+
   try {
     const page = await client.fetch(query, { slug: slug[0] });
     return {
       props: {
+        headerLinks: headerLinks,
         page: page || {},
       },
     };
   } catch {
     const page = await client.fetch(query, { slug: "home-page" });
+    const header = await client.fetch(headerQuery);
+    const headerLinks = header[0].links;
     return {
       props: {
-        page: page || {},
+        headerLinks: headerLinks,
+        page: page,
       },
     };
   }
